a262f62ebb6b8b866d9a88c130f617efb69f5b1e
Linh Nguyen <linh.nguyen@enway.ai>
2023-01-03 10:05:44 +0800

    Clean up test files

--- src/road_boundary/process_lane.cpp
+++ src/road_boundary/process_lane.cpp
@@ -3,24 +3,36 @@
 #include <opencv2/imgproc.hpp>
 #include <ostream>
 #include <unistd.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/yaml.h>
 
 int
 main(int argc, char** argv)
 {
-  auto image = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
+  const auto yaml = YAML::LoadFile(std::string(argv[1]));
+  auto image = cv::imread(yaml["image_path"].as<std::string>(), cv::IMREAD_UNCHANGED);
   cv::Mat eroded_image;
-  cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(7, 7), cv::Point(3, 3));
+  auto element_size = yaml["erode"]["size"].as<int>();
+  auto point = yaml["erode"]["point"].as<int>();
+  cv::Mat element =
+      cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(element_size, element_size), cv::Point(point, point));
   cv::erode(image, eroded_image, element);
-  // cv::imwrite("/home/linh/eroded_image.png", eroded_image);
-  auto lane_image = cv::imread(argv[2], cv::IMREAD_UNCHANGED);
+
+  auto lane_image = cv::imread(yaml["lane_image_path"].as<std::string>(), cv::IMREAD_UNCHANGED);
   cv::Mat lane_image_resized;
   cv::resize(lane_image, lane_image_resized, eroded_image.size());
-  cv::Mat lane_image_masked(lane_image_resized.size(), lane_image_resized.type(), cv::Scalar(0, 0, 0, 255 * 255));
+  cv::Mat lane_image_masked(
+      lane_image_resized.size(), lane_image_resized.type(), cv::Scalar(0, 0, 0, yaml["masked_alpha"].as<int>()));
   lane_image_resized.copyTo(lane_image_masked, eroded_image);
+
   cv::Mat lane_binary;
   cv::cvtColor(lane_image_masked, lane_binary, cv::COLOR_BGRA2GRAY);
   cv::Mat lane_threshold;
-  cv::threshold(lane_binary, lane_threshold, 255 * 10.0, 255.0 * 255.0, cv::THRESH_BINARY);
+  cv::threshold(lane_binary,
+                lane_threshold,
+                yaml["threshold"]["value"].as<int>(),
+                yaml["threshold"]["max"].as<int>(),
+                cv::THRESH_BINARY);
   cv::Mat lane_threshold_type(lane_threshold.size(), CV_8UC1);
   lane_threshold.convertTo(lane_threshold_type, CV_8UC1, 255.0);
   std::vector<std::vector<cv::Point>> contours;
@@ -28,34 +40,42 @@ main(int argc, char** argv)
   cv::findContours(lane_threshold_type, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
   std::cout << "Found " << contours.size() << " contours" << std::endl;
   std::vector<std::vector<cv::Point>> chosen;
-  std::vector<std::vector<cv::Point>> weaker;
   for (auto contour : contours)
   {
     auto area = cv::contourArea(contour);
-    if (area > 60)
+    if (area > yaml["contour"]["min_area"].as<double>())
     {
       chosen.push_back(contour);
     }
   }
   std::cout << "Drawing " << chosen.size() << " contours" << std::endl;
-  std::cout << "Lane lines " << weaker.size() << std::endl;
   cv::drawContours(image, chosen, -1, 0, -1);
   cv::Mat eroded_extra;
   cv::erode(image, eroded_extra, element);
-  cv::imwrite("/home/linh/image_with_extra_lanes.png", eroded_extra);
+  if (!yaml["save_extra_lane"].as<std::string>().empty())
+  {
+    cv::imwrite(yaml["save_extra_lane"].as<std::string>(), eroded_extra);
+  }
 
   cv::Mat dilated;
   cv::dilate(eroded_image, dilated, element);
-
-  cv::drawContours(dilated, chosen, -1, cv::Scalar(0, 0, 0, 255 * 255), -1);
-  // cv::drawContours(dilated, weaker, -1, cv::Scalar(255 * 0, 255 * 0, 255 * 0, 255 * 255), -1);
+  cv::drawContours(dilated, chosen, -1, cv::Scalar(0, 0, 0, yaml["masked_alpha"].as<int>()), -1);
 
   cv::Mat blurred;
-  cv::GaussianBlur(dilated, blurred, cv::Size(3, 3), 0);
+  auto blur_size = yaml["blur"]["kernel"].as<int>();
+  cv::GaussianBlur(dilated, blurred, cv::Size(blur_size, blur_size), 0);
   cv::Mat edge;
-  cv::Laplacian(blurred, edge, CV_8U, 3, 1, 0);
-  cv::Mat combined = edge + 0.5 * lane_threshold_type;
-  cv::imwrite("/home/linh/inner_region_with_extra.png", combined);
-
+  cv::Laplacian(blurred,
+                edge,
+                CV_8U,
+                yaml["edge"]["size"].as<int>(),
+                yaml["edge"]["scale"].as<int>(),
+                yaml["edge"]["delta"].as<int>());
+  cv::Mat combined = edge + yaml["lane_alpha"].as<double>() * lane_threshold_type;
+  auto saved_image = yaml["save_image"].as<std::string>();
+  if (!saved_image.empty())
+  {
+    cv::imwrite(saved_image, combined);
+  }
   return 0;
 }
--- src/road_boundary/test_map.cpp
+++ src/road_boundary/test_map.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <lanelet2_core/Forward.h>
-#include <lanelet2_core/primitives/Point.h>
 #include <lanelet2_io/Io.h>
 #include <lanelet2_io/Projection.h>
 #include <lanelet2_io/io_handlers/OsmHandler.h>
@@ -8,98 +7,75 @@
 #include <lanelet2_matching/Types.h>
 #include <lanelet2_projection/UTM.h>
 
+#include <road_boundary/helper.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/yaml.h>
+
 int
 main(int argc, char** argv)
 {
+  const auto yaml = YAML::LoadFile(std::string(argv[1]));
+  auto map_origin = readMapOrigin(yaml["map_origin"].as<std::string>());
   lanelet::ErrorMessages errors;
-  lanelet::Origin origin({1.3541351067 - 0.0015, 103.695233561 + 0.00491});
+  lanelet::Origin origin({map_origin.center.x, map_origin.center.y});
   auto projector = lanelet::projection::UtmProjector(origin);
-  // lanelet::Origin origin({49.0, 8.4});
   lanelet::io_handlers::OsmParser parser(projector);
-  // lanelet::LaneletMapPtr map = lanelet::load("/home/linh/.ros/test_osm_nodes.osm", origin, &errors);
-  auto map = parser.parse("/home/linh/.ros/test_osm_nodes.osm", errors);
+  auto map = parser.parse(yaml["map"].as<std::string>(), errors);
   if (errors.empty())
   {
     std::cout << "Loaded successfully" << std::endl;
-    std::cout << map->size() << std::endl;
-    std::cout << map->laneletLayer.size() << std::endl;
+    std::cout << "Map size " << map->size() << std::endl;
+    std::cout << "Lanelet layer size " << map->laneletLayer.size() << std::endl;
   }
 
-  lanelet::matching::Object2d query;
-  query.pose.translation() = lanelet::BasicPoint2d {0, 0};
-  query.pose.linear() = Eigen::Rotation2D<double>(0.0).matrix();
+  auto resolution = yaml["resolution"].as<double>();
 
-  auto matching = lanelet::matching::getDeterministicMatches(*map, query, 5000.0);
-  std::cout << matching.size() << std::endl;
-  for (const auto m : matching)
+  auto image_size = yaml["image_size"].as<int>();
+  auto points = parseBagPath(yaml["test_bag_path"].as<std::string>(),
+                             cv::Size(image_size, image_size),
+                             resolution,
+                             yaml["topic"].as<std::string>());
+
+  bool first_point = false;
+  cv::Point2i last_point;
+  int count = 0;
+
+  double angle = map_origin.angle;
+  double cos = std::cos(angle * M_PI / 180.0);
+  double sin = std::sin(angle * M_PI / 180.0);
+
+  for (const auto index : points)
   {
-    std::cout << m.lanelet.id() << " " << m.distance << std::endl;
+    if (!first_point)
+    {
+      last_point = index;
+      first_point = true;
+    }
+    if (distance(index, last_point) < yaml["query_distance"].as<double>() / resolution)
+    {
+      continue;
+    }
+    // auto gpspoint = projector.reverse(lanelet::BasicPoint3d(
+    //     -(index.y * cos + index.x * sin) * resolution, -(index.x * cos - index.y * sin) * resolution, 0));
+    // auto normal_point = projector.forward(gpspoint);
+    lanelet::matching::Object2d query;
+    query.pose.translation() = lanelet::BasicPoint2d {-(index.y * cos + index.x * sin) * resolution,
+                                                      -(index.x * cos - index.y * sin) * resolution};
+    query.pose.linear() = Eigen::Rotation2D<double>(0.0).matrix();
+
+    auto matching = lanelet::matching::getDeterministicMatches(*map, query, yaml["max_distance"].as<double>());
+    if (matching.size() > 0)
+    {
+      std::cout << "Point " << index.x << " " << index.y << " matches " << matching[0].lanelet.id() << std::endl;
+    }
+    else
+    {
+      std::cout << "Point " << index.x << " " << index.y << " has no match\n";
+    }
+    last_point = index;
+    count++;
   }
 
-  auto nearest = map->laneletLayer.nearest(lanelet::BasicPoint2d(0, 0), 1);
-  std::cout << nearest[0].id() << std::endl;
-
-  for (const auto data : nearest[0].rightBound2d())
-  {
-    std::cout << data.x() << " " << data.y() << std::endl;
-  }
-
+  std::cout << "Processed " << count << std::endl;
   return 0;
-  // lanelet::ErrorMessages errors;
-  // lanelet::Origin origin({1.3541351067 - 0.0015, 103.695233561 + 0.00491});
-  // auto projector = lanelet::projection::UtmProjector(origin);
-  // // lanelet::Origin origin({49.0, 8.4});
-  // lanelet::io_handlers::OsmParser parser(projector);
-  // // lanelet::LaneletMapPtr map = lanelet::load("/home/linh/.ros/test_osm_nodes.osm", origin, &errors);
-  // auto map = parser.parse("/home/linh/.ros/test_osm_nodes.osm", errors);
-  // double angle = 62;
-  // double cos = std::cos(angle * M_PI / 180.0);
-  // double sin = std::sin(angle * M_PI / 180.0);
-
-  // first_point = false;
-  // int count = 0;
-  // for (rosbag::MessageInstance const message : rosbag::View(bag, rosbag::TopicQuery(topic)))
-  // {
-  //   const auto position = message.instantiate<nav_msgs::Odometry>();
-  //   const auto index = road_boundary_.convertToIndex(position->pose.pose.position.x, position->pose.pose.position.y);
-  //   if (!first_point)
-  //   {
-  //     last_point = index;
-  //     first_point = true;
-  //     last_time = position->header.stamp;
-  //   }
-  //   if ((position->header.stamp - last_time).toSec() < 2.5)
-  //   {
-  //     continue;
-  //   }
-  //   if (std::abs(index.x - last_point.x) + std::abs(index.y - last_point.y) < 500)
-  //   {
-  //     continue;
-  //   }
-  //   auto gpspoint = projector.reverse(
-  //       lanelet::BasicPoint3d(-(index.y * cos + index.x * sin) * 0.1, -(index.x * cos - index.y * sin) * 0.1, 0));
-  //   std::cout << -(index.y * cos + index.x * sin) * 0.1 << " " << -(index.x * cos - index.y * sin) * 0.1 << " vs ";
-  //   auto normal_point = projector.forward(gpspoint);
-  //   std::cout << normal_point.x() << " " << normal_point.y() << std::endl;
-  //   lanelet::matching::Object2d query;
-  //   // query.pose.translation() = lanelet::BasicPoint2d {normal_point.x(), normal_point.y()};
-  //   query.pose.translation() =
-  //       lanelet::BasicPoint2d {-(index.y * cos + index.x * sin) * 0.1, -(index.x * cos - index.y * sin) * 0.1};
-  //   query.pose.linear() = Eigen::Rotation2D<double>(0.0).matrix();
-
-  //   auto matching = lanelet::matching::getDeterministicMatches(*map, query, 50.0);
-  //   if (matching.size() > 0)
-  //   {
-  //     road_boundary_.drawId(index, matching[0].lanelet.id());
-  //   }
-  //   else
-  //   {
-  //     std::cout << "No match\n";
-  //   }
-  //   last_point = index;
-  //   last_time = position->header.stamp;
-  //   count++;
-  // }
-
-  // std::cout << "Processed " << count << std::endl;
 }
